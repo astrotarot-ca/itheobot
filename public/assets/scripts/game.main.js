81e94cdb3a8d44d6ebda17db88014bec5f39de0f
Seraph DOs Santos <cyb3rsh4t@gmail.com>
2019-07-07 17:24:32 -0400

    now touch friendly

--- public/assets/scripts/game.main.js
+++ public/assets/scripts/game.main.js
@@ -8,83 +8,97 @@ var sCS;	// card size on appear
 var sCB;	// cardback
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var active = false;
+  var currentX;
+  var currentY;
+  var initialX;
+  var initialY;
+  var xOffset = 0;
+  var yOffset = 0;
   
-  elmnt.onmousedown = dragMouseDown;
-  elmnt.ontouchstart = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-
+  elmnt.addEventListener("touchstart", dragStart, false);
+  elmnt.addEventListener("touchend", dragEnd, false);
+  elmnt.addEventListener("touchmove", drag, false);
+  
+  elmnt.addEventListener("mousedown", dragStart, false);
+  elmnt.addEventListener("mouseup", dragEnd, false);
+  elmnt.addEventListener("mousemove", drag, false);
+  
+  function dragStart(e) {
     if (elmnt.src.includes(sCB) && iImgs>iImgsIdx) {
         elmnt.src = aRndCrds.shift();
         iImgsIdx++;
     }
-
+  
     elmnt.style.width = sCS;
     elmnt.style.zIndex+=1;
-
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.ontouchend = closeDragElement;
-
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
+  
+    if (e.type === "touchstart") {
+      initialX = e.touches[0].clientX - xOffset;
+      initialY = e.touches[0].clientY - yOffset;
+    } else {
+      initialX = e.clientX - xOffset;
+      initialY = e.clientY - yOffset;
+    }
+  
+    if (e.target === elmnt) {
+      active = true;
+    }
   }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  
+  function dragEnd(e) {
+    initialX = currentX;
+    initialY = currentY;
+  
+    active = false;
   }
+  
+  function drag(e) {
+    if (active) {
+    
+      e.preventDefault();
+    
+      if (e.type === "touchmove") {
+        currentX = e.touches[0].clientX - initialX;
+        currentY = e.touches[0].clientY - initialY;
+      } else {
+        currentX = e.clientX - initialX;
+        currentY = e.clientY - initialY;
+      }
+  
+      xOffset = currentX;
+      yOffset = currentY;
+  
+      setTranslate(currentX, currentY, elmnt);
+    }
+  }  
+}
 
-  function closeDragElement() {
-		// stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.ontouchend = null;
-    document.onmousemove = null;
-    document.ontouchmove = null;
-  }
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
+  return array;
+}
   
 function pullNext(sGameId,sCardBack,sImgAlt) {
-
-    oNewImg = new Image();
-    
-    $(oNewImg).attr({
-        src: "../../assets/images/cards/"+sGameId+"/w300px/"+sCardBack,
-        alt: sImgAlt,
-        class: "card"
-    });
-    dragElement(oNewImg);
-    $("#mainGameBoard").append(oNewImg);
-    
+  oNewImg = new Image();
+  
+  $(oNewImg).attr({
+      src: "../../assets/images/cards/"+sGameId+"/w300px/"+sCardBack,
+      alt: sImgAlt,
+      class: "card"
+  });
+  dragElement(oNewImg);
+  $("#mainGameBoard").append(oNewImg);
 }
