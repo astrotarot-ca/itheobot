74e23cd2878723518ddc279ed0bbb3a869ca640a
Seraph DOs Santos <cyb3rsh4t@gmail.com>
2019-07-07 17:44:58 -0400

    touch friendly

--- public/assets/scripts/oracle/zo/game.main.js
+++ public/assets/scripts/oracle/zo/game.main.js
@@ -28,52 +28,80 @@ function shuffle(array) {
 }
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
-  elmnt.ontouchstart = dragMouseDown;
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-		if (iImgs>iImgsIdx) {
+  var active = false;
+  var currentX;
+  var currentY;
+  var initialX;
+  var initialY;
+  var xOffset = 0;
+  var yOffset = 0;
+  
+  elmnt.addEventListener("touchstart", dragStart, false);
+  elmnt.addEventListener("touchend", dragEnd, false);
+  elmnt.addEventListener("touchmove", drag, false);
+  
+  elmnt.addEventListener("mousedown", dragStart, false);
+  elmnt.addEventListener("mouseup", dragEnd, false);
+  elmnt.addEventListener("mousemove", drag, false);
+  
+  function dragStart(e) {
+    if (iImgs>iImgsIdx) {
       if (elmnt.src.includes(sCBP)) {
         elmnt.src = aRndCrdsP.shift();
       } else if (elmnt.src.includes(sCBC)) {
         elmnt.src = aRndCrdsC.shift();
-      } else {
+      } else if (elmnt.src.includes(sCBH)) {
         elmnt.src = aRndCrdsH.shift();
       }
+      iImgsIdx++;
 	  }
-		elmnt.style.width = sCS;
-		elmnt.style.zIndex += 10;
+  
+    elmnt.style.width = sCS;
+    elmnt.style.zIndex+=1;
+  
+    if (e.type === "touchstart") {
+      initialX = e.touches[0].clientX - xOffset;
+      initialY = e.touches[0].clientY - yOffset;
+    } else {
+      initialX = e.clientX - xOffset;
+      initialY = e.clientY - yOffset;
+    }
+  
+    if (e.target === elmnt) {
+      active = true;
+    }
+  }
+  
+  function dragEnd(e) {
+    initialX = currentX;
+    initialY = currentY;
+  
+    active = false;
+  }
+  
+  function drag(e) {
+    if (active) {
+    
+      e.preventDefault();
+    
+      if (e.type === "touchmove") {
+        currentX = e.touches[0].clientX - initialX;
+        currentY = e.touches[0].clientY - initialY;
+      } else {
+        currentX = e.clientX - initialX;
+        currentY = e.clientY - initialY;
+      }
+  
+      xOffset = currentX;
+      yOffset = currentY;
+  
+      setTranslate(currentX, currentY, elmnt);
+    }
+  }  
+}
 
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.ontouchend = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
-  }
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-  function closeDragElement() {
-		// stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.ontouchend = null;
-    document.onmousemove = null;
-    document.ontouchmove = null;
-  }
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
 
 function pullNextZoP(sGameId,sCardBackP,sImgAlt) {
