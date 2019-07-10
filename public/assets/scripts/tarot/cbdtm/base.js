f55638dc14343038a8e40e35cf66bda28f91becd
Seraph DOs Santos <cyb3rsh4t@gmail.com>
2019-07-10 00:38:36 -0400

    chaged card layout and added deck selector

--- public/assets/scripts/tarot/cbdtm/base.js
+++ public/assets/scripts/tarot/cbdtm/base.js
@@ -4,7 +4,7 @@
 iImgs = 78;         // number of cards in deck
 sCB = "cardback.jpg";   // cardback id
 
-function populateArray(aCrds) {
+function populateDeckAL(aCrds) {
 	for (var j=1;j<=22;j++) {
 		aCrds.push("../../assets/images/cards/cbdtm/w300px/A"+j+".jpg");
 	}
@@ -17,7 +17,24 @@ function populateArray(aCrds) {
 	return aCrds;
 }
 
+function populateDeckMA(aCrds) {
+	for (var j=1;j<=22;j++) {
+		aCrds.push("../../assets/images/cards/cbdtm/w300px/A"+j+".jpg");
+	}
+	return aCrds;
+}
+function populateDeckMI(aCrds) {
+	for (var i=1;i<=14;i++) {
+		aCrds.push("../../assets/images/cards/cbdtm/w300px/S"+i+".jpg");
+		aCrds.push("../../assets/images/cards/cbdtm/w300px/C"+i+".jpg");
+		aCrds.push("../../assets/images/cards/cbdtm/w300px/W"+i+".jpg");
+		aCrds.push("../../assets/images/cards/cbdtm/w300px/D"+i+".jpg");
+	}
+	return aCrds;
+}
+
 $(function () {
-    aRndCrds = populateArray(aRndCrds)
+	aRndCrds = new Array();
+    aRndCrds = populateDeckAL(aRndCrds)
     aRndCrds = shuffle(aRndCrds);
 });
